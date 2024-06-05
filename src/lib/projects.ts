b643d33de5b63407a1718e48e1956b41ed519939
Jcho114 <108897459+Jcho114@users.noreply.github.com>
2024-06-05 17:49:41 -0400

    added file for project descriptions

---
+++ src/lib/projects.ts
@@ -0,0 +1,9 @@
+import type Project from "./types/Project";
+import Tag from './types/Tag';
+
+export const portfolio: Project = {
+    name: "Portfolio",
+    description: "Personal Website",
+    role: "Web Developer",
+    tags: [Tag.TypeScript, Tag.Svelte, Tag.Vitest],
+};
