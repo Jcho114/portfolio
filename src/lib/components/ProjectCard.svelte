2fdb53435c4e52c0d2cd31e08261041477455a3a
Jcho114 <108897459+Jcho114@users.noreply.github.com>
2024-06-05 17:56:42 -0400

    tailwind

--- src/lib/components/ProjectCard.svelte
+++ src/lib/components/ProjectCard.svelte
@@ -4,11 +4,13 @@
     export let project: Project;
 </script>
 
-<h4>{project.name}</h4>
-<h4>{project.description}</h4>
-<h4>{project.role}</h4>
-<div>
-    {#each project.tags as tag}
-        <h4>{tag}</h4>
-    {/each}
+<div class="flex flex-col gap-1">
+    <p>{project.name}</p>
+    <p>{project.description}</p>
+    <p>{project.role}</p>
+    <div class="flex gap-4">
+        {#each project.tags as tag}
+            <p>{tag}</p>
+        {/each}
+    </div>
 </div>
