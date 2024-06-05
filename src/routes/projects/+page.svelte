418ad628e7ab6f45f9aadb3af4fcacbb1459db27
Jcho114 <108897459+Jcho114@users.noreply.github.com>
2024-06-05 17:50:10 -0400

    added project routes

--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -1,1 +1,7 @@
+<script type="ts">
+    import ProjectCard from "$lib/components/ProjectCard.svelte";
+	import { portfolio } from "$lib/projects";
+</script>
+
 <h1>Projects</h1>
+<ProjectCard project={portfolio} />
--- src/routes/projects/test/+page.svelte
+++ src/routes/projects/test/+page.svelte
@@ -1,7 +1,2 @@
 <script type="ts">
-    import ProjectCard from "$lib/components/ProjectCard.svelte";
-
-    let projectName="Test"
 </script>
-
-<ProjectCard {projectName} />
