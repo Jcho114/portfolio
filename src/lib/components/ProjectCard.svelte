9a2f7c1ceeadb1e93ec9374befb76bb039c7bc2b
Jcho114 <108897459+Jcho114@users.noreply.github.com>
2024-06-04 21:42:21 -0400

    svelte component

---
+++ src/lib/components/ProjectCard.svelte
@@ -0,0 +1,5 @@
+<script lang="ts">
+    export let projectName: string;
+</script>
+
+<h1>{projectName}</h1>
---
+++ src/routes/projects/test/+page.svelte
@@ -0,0 +1,7 @@
+<script type="ts">
+    import ProjectCard from "$lib/components/ProjectCard.svelte";
+
+    let projectName="Test"
+</script>
+
+<ProjectCard {projectName} />
