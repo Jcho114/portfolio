f64d5a91e436cf4a59ce067afdcf66a79dab5d21
Jcho114 <108897459+Jcho114@users.noreply.github.com>
2024-06-07 19:47:53 -0400

    more ui updates

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -7,7 +7,9 @@
 	<main class="h-[15rem] flex flex-col justify-center items-center gap-3">
 		<div class="flex justify-center items-center gap-3 text-3xl">
 			<a href="https://github.com/Jcho114" target="_blank"><Icon icon="mdi:github" /></a>
-			<a href="https://www.linkedin.com/in/jcho1114/" target="_blank"><Icon icon="mdi:linkedin" /></a>
+			<a href="https://www.linkedin.com/in/jcho1114/" target="_blank"
+				><Icon icon="mdi:linkedin" /></a
+			>
 			<a href="mailto:joseph.cho20@gmail.com"><Icon icon="mdi:email" /></a>
 		</div>
 	</main>
--- src/lib/components/Home.svelte
+++ src/lib/components/Home.svelte
@@ -1,3 +1,14 @@
-<div class="flex justify-center items-center h-full-with-navbar">
-	<p class="text-5xl font-semibold">Home</p>
+<script lang="ts">
+</script>
+
+<div class="flex justify-center items-center h-full-with-navbar gap-x-44">
+	<div class="flex flex-col justify-center gap-0.5">
+		<p class="text-8xl font-semibold">Joseph Cho</p>
+		<div class="h-5" />
+		<p class="text-xl text-gray-300">Full Stack Developer</p>
+		<p class="text-xl text-gray-300">Computer Science and Statistics at University of Maryland</p>
+		<div class="h-6" />
+		<p class="border w-fit px-4 py-1 rounded-full text-lg text-gray-300 hover">Scroll ↓</p>
+	</div>
+	<div class="rounded w-1/4 h-[60vh] bg-white"></div>
 </div>
