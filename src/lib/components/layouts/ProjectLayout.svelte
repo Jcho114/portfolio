9e97cad63574b929beb3637d4f488634f147793a
Jcho114 <108897459+Jcho114@users.noreply.github.com>
2024-06-08 16:36:51 -0400

    added markdown with mdsvex

---
+++ src/lib/components/layouts/ProjectLayout.svelte
@@ -0,0 +1,3 @@
+<main class="w-inherit min-h-full-with-navbar flex flex-col justify-center items-center">
+	<slot />
+</main>
--- src/routes/projects/portfolio/+page.svelte
+++
@@ -1,3 +0,0 @@
-<main class="w-inherit min-h-full-with-navbar flex justify-center items-center">
-	<h1>Portfolio</h1>
-</main>
