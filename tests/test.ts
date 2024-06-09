b386a285d2a2fe56ad7444d2594b178f51608ed9
Jcho114 <108897459+Jcho114@users.noreply.github.com>
2024-06-08 21:15:46 -0400

    filler e2e test

--- tests/test.ts
+++ tests/test.ts
@@ -1,6 +1,6 @@
 import { expect, test } from '@playwright/test';
 
-test('home page has expected h1', async ({ page }) => {
+test('filler', async ({ page }) => {
 	await page.goto('/');
 	await expect(true).toBeTruthy();
 });
