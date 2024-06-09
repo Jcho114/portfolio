edb8b034f9d9ee226b13cad4d888e2cb9862c6b0
Jcho114 <108897459+Jcho114@users.noreply.github.com>
2024-06-09 17:18:07 -0400

    remove .netlify

--- .netlify/server/nodes/3.js
+++
@@ -1,8 +0,0 @@
-
-
-export const index = 3;
-let component_cache;
-export const component = async () => component_cache ??= (await import('../entries/pages/projects/appdevclub/_page.svx.js')).default;
-export const imports = ["_app/immutable/nodes/3.CFw6cvDo.js","_app/immutable/chunks/scheduler.K7eN4_uM.js","_app/immutable/chunks/index.BjNOq-62.js","_app/immutable/chunks/spread.CgU5AtxT.js","_app/immutable/chunks/ProjectLayout.BWzyP7oL.js","_app/immutable/chunks/projects.sdD-MCuB.js"];
-export const stylesheets = [];
-export const fonts = [];
